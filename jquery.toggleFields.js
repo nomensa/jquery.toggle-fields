06e72e6c14953bfc714f6395db729200aca6c6ed
Andrew Nevins <anevins@nomensa.com>
2016-12-07 16:41:16 +0000

    added recursion for disabled fields
    attempted recursive enabled state
    
    half way to figuring out recursion
    
    added recursion

--- jquery.toggleFields.js
+++ jquery.toggleFields.js
@@ -1,21 +1,14 @@
-/*
- * Toggles an disabled or enabled state on form fields
- *
- * Requires the following DOM attributes:
- * * [data-toggle-conditions="#field-1-with-condition, #field-2-with-condition"] - Required: List of condition IDs, prepended with a hash, comma separated.
- * * [data-toggle-target] - Required: The form field to toggle.
- * * [data-toggle-next] - Optional: The next form row to toggle
- * * [data-toggle-ref="#field-1-with-condition"] - Required if using 'data-toggle-next': Identifies which condition it relates to
-*/
 'use strict';
 
 var pluginName = 'toggleFields',
     defaults,
     counter = 0,
+    conditionIdentifier = 'data-toggle-condition',
     conditionsIdentifier = 'data-toggle-conditions',
     conditions = $('[' + conditionsIdentifier + ']'),
     helpTextIdentifier = 'form-row__instructions',
-    nextFormRowsIdentifier = '[data-toggle-next]',
+    nextFormRowsIdentifier = 'data-toggle-next',
+    nextRowReferenceIdentifier = 'data-toggle-ref',
     defaults = {
         initCallback: function initCallback() {},
         destroyCallback: function destroyCallback() {},
@@ -24,9 +17,14 @@ var pluginName = 'toggleFields',
         toggleOffCallback: function toggleOffCallback() {}
     },
     disabledClass = 'disabled',
-    disabledAttr = 'disabled';
+    disabledAttr = 'disabled',
+    targetIdentifier = 'data-toggle-target';
 
-function toggleFields(options) {
+/* 
+ * toggleFields
+ * Enables logic to show or enable form fields when conditions are met.
+ */
+function toggleFields() {
     /*
         Constructor function for the toggle fields plugin
     */
@@ -43,11 +41,13 @@ function toggleFields(options) {
         conditionTargets.each(function() {
             var condition = $(this),
                 fieldToWatch,
-                nextFormRows = $(nextFormRowsIdentifier),
-                nextRowReferenceIdentifier = 'data-toggle-ref',
+                nextFormRows = $('[' + nextFormRowsIdentifier + ']'),
                 radioNameIdentifier,
                 targets,
                 wrapper = condition.parents('form');
+
+            // Add an identifier to the condition
+            condition.attr(conditionIdentifier, '');
 
             // If specified next form row
             if (nextFormRows.length !== 0) {
@@ -61,7 +61,7 @@ function toggleFields(options) {
             }
 
             // Get all target fields from the row
-            targets = nextFormRows.find('[data-toggle-target], .' + helpTextIdentifier);
+            targets = nextFormRows.find('[' + targetIdentifier + '], .' + helpTextIdentifier);
 
             // If there are no form fields
             if (targets.length === 0) {
@@ -98,9 +98,12 @@ function toggleFields(options) {
 
 /*
  * Handles the disabled and enabled states
+ * @param {jQueryObject} condition - The trigger for the condition
+ * @param {jQueryObject} targets - The form fields to be toggled
  */
 function toggleFields__applyToggle(condition, targets) {
-    var options = $.extend({}, defaults, options);
+    var options = $.extend({}, defaults, options),
+        conditionContainer;
 
     condition = $(condition);
     targets = $(targets);
@@ -108,22 +111,28 @@ function toggleFields__applyToggle(condition, targets) {
     // Initially disable all targets
     targets.each(function() {
         var target = $(this),
-            container = target.parents(nextFormRowsIdentifier);
+            container = target.parents('[' + nextFormRowsIdentifier + ']');
 
         // If the condition has been selected
         if (condition.is(':selected') || condition.is(':checked')) {
+
             // If the target is an input field
             if (target.is('input') || target.is('select')) {
                 // Remove the disable state
                 target.removeAttr(disabledAttr);
             }
+
             // Remove the disabled class to the target
             target.removeClass(disabledClass);
             // Remove toggle class to the container
             container.removeClass(options.toggleClass);
+            // Apply recursion when necessary
+            recursiveToggle(container);
             // Call the toggleOffCallback
             options.toggleOffCallback();
-        } else {
+        } 
+        // If the condition has not been selected
+        else {
             // Disable the targets if they have not already been disabled
             if (!target.attr(disabledAttr) || !target.hasClass(disabledClass)) {
                 // If the target is an input field
@@ -133,12 +142,44 @@ function toggleFields__applyToggle(condition, targets) {
                 }
                 // Add a disabled class
                 target.addClass(disabledClass);
+                // Apply recursive logic
+                container.addClass(options.toggleClass);
+                // Call the toggleOnCallback
+                options.toggleOnCallback();
             }
 
-            // Apply toggle class to the container
-            container.addClass(options.toggleClass);
-            // Call the toggleOnCallback
-            options.toggleOnCallback();
+            // Apply recursion when necessary
+            recursiveToggle(container, condition);
         }
     });
+}
+
+/*
+ * To re-toggle recursive conditions if they have already been met
+ * @param {jQueryObject} container - The containing element of the targets
+ * @param {jQueryObject} condition - The trigger for the condition before recursion
+ */
+function recursiveToggle(container, condition) {
+    var recursiveIdentifer = 'data-toggle-recursive';
+
+    // If the container is recursive
+    if (container.is('[' + recursiveIdentifer + ']')) {
+        var nextRecursiveContainer = container.next('[' + recursiveIdentifer + ']'),
+            nextRecursiveTargets =  nextRecursiveContainer.find('[' + targetIdentifier + '], .' + helpTextIdentifier);
+
+        // If not using the condition before recursion
+        if (!condition) {
+            // Use the recursive condition
+            var condition = container.find('[' + conditionIdentifier + ']');
+        }
+
+        // If there are no form fields
+        if (nextRecursiveTargets.length === 0) {
+            // Set the targets to be the nextFormRow container
+            nextRecursiveTargets = nextRecursiveContainer;
+        }
+
+        // Re-run the logic if the recursive conditions have already been met
+        toggleFields__applyToggle(condition, nextRecursiveTargets);
+    }
 }
