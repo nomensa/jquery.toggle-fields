c31c6aeb55723addaa0df240920e9d2fd9a2c894
Andrew Nevins <anevins@nomensa.com>
2017-08-02 13:25:14 +0100

    cleaned up unnecessary grunt commands

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,106 +11,14 @@ module.exports = function(grunt) {
     // Project configuration
     grunt.initConfig({
 
-        //Start a connect web server
-        connect: {
-            server: {
-                options: {
-                    livereload: 35729,
-                    hostname: '0.0.0.0',
-                    port: 9001
-                }
-            }
-        },
-
-        // Task for checking JavaScript Code Style with jscs
-        jscs: {
-            options: {
-                config: '.jscsrc'
-            },
-            files: [
-                'Gruntfile.js',
-                'jquery.toggleFields.js'
-            ]
-        },
-
-        // Minify files with UglifyJS
-        uglify: {
-            options: {
-                preserveComments: 'some'
-            },
-            target: {
-                files: {
-                    'jquery.toggleFields.min.js': ['jquery.toggleFields.js']
-                }
-            }
-        },
-
         // Grunt plugin for Karma
         karma: {
             all: {
                 configFile: 'karma.conf.js'
             }
-        },
-
-        watch: {
-            options: {
-                debounceDelay: 250,
-                livereload: 35729
-            },
-            html: {
-                files: [
-                    'index.html',
-                ]
-            },
-            js: {
-                files: [
-                    'jquery.toggleFields.js',
-                ],
-                tasks: [
-                    'uglify'
-                ]
-            },
-            test: {
-                files: [
-                    'Gruntfile.js',
-                    'jquery.toggleFields.js',
-                    'jquery.toggleFields.spec.js'
-                ],
-                tasks: [
-                    'karma'
-                ]
-            }
-        },
-        version: {
-            options: {
-                prefix: '@version:\\s+[\'"]'
-            },
-            src: [
-                'jquery.toggleFields.js',
-                'jquery.toggleFields.min.js'
-            ]
         }
-    });
 
-    grunt.registerTask('default', 'watch (@options: --connect)', function() {
-    /**
-     * Defaut task
-     * $ grunt
-     * @options: --connect (run watch with a connect web server)
-     */
-        if ( grunt.option('connect') ) {
-            grunt.task.run([
-                'connect',
-                'watch'
-            ]);
-        } else {
-            grunt.task.run([
-                'watch'
-            ]);
-        }
     });
 
     grunt.registerTask('test', ['karma']);
-
-    grunt.registerTask('release', ['test', 'version']);
 };
