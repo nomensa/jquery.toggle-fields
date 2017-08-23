6d2717074cb8926706faeb6aec4659119636e426
Andrew Nevins <anevins@nomensa.com>
2017-08-23 11:50:05 +0100

    TTD-28: Added jQuery pattern

--- jquery.toggleFields.js
+++ jquery.toggleFields.js
@@ -1,7 +1,18 @@
-function toggleFields(userOptions) {
+/*
+*  jquery.pardotA11y.js
+*  Adding accessibility to Pardot forms
+*
+*  Made by Nomensa
+*  Under MIT License
+*/
+;(function($, window, document, undefined) {
     'use strict';
 
-    var defaults = {
+    // Create the defaults once
+    var pluginName = 'toggleFields',
+        defaults = {
+            // Used on attributes added by the plugin
+            namespace: 'toggle-fields',
             conditionIdentifier: 'data-toggle-condition',
             conditionsIdentifier: 'data-toggle-conditions',
             disabledClass: 'disabled',
@@ -10,178 +21,194 @@ function toggleFields(userOptions) {
             nextFormRowsIdentifier: 'data-toggle-next',
             nextRowReferenceIdentifier: 'data-toggle-ref',
             targetIdentifier: 'data-toggle-target',
-            toggleClass: 'js-toggleFields--on'
-        },
-        callbacks = {
+            toggleClass: 'js-toggleFields--on',
             initCallback: function initCallback() {},
             toggleOnCallback: function toggleOnCallback() {},
             toggleOffCallback: function toggleOffCallback() {}
         };
 
-    init();
+    // The plugin constructor
+    function ToggleFields(element, options) {
+        // The form element
+        this.element = $(element);
+        // Extending the options
+        this.settings = $.extend({}, defaults, options);
+        // Store the plugin name
+        this.name = pluginName;
 
-    /* 
-     * Initialisation
-     * Enables logic to show or enable form fields when conditions are met.
-     */
-    function init() {
-        /*
-            Constructor function for the toggle fields plugin
-        */
-        var options = $.extend({}, callbacks, defaults, userOptions),
-            conditions = $('[' + options.conditionsIdentifier + ']');
-
-        if (conditions.length !== 0) {
-            var conditionValues = conditions.attr(options.conditionsIdentifier),
-                conditionTargets = $(conditionValues);
-
-            // Init callback
-            options.initCallback();
-
-            // For each condition
-            conditionTargets.each(function() {
-                var condition = $(this),
-                    fieldToWatch,
-                    nextFormRows = $('[' + options.nextFormRowsIdentifier + ']'),
-                    radioNameIdentifier,
-                    targets,
-                    wrapper = condition.parents('form');
-
-                // Add an identifier to the condition
-                condition.attr(options.conditionIdentifier, '');
-
-                // If specified next form row
-                if (nextFormRows.length !== 0) {
-                    // Only match next rows for the condition
-                    nextFormRows = nextFormRows.filter('[' + options.nextRowReferenceIdentifier + '="' + condition.attr('id') + '"]');
-                } else {
-                    // Otherwise we assume the target is the next form row
-                    nextFormRows = condition.parents().next();
-                    // Add the options.nextFormRowsIdentifier
-                    nextFormRows.attr(options.nextFormRowsIdentifier, '');
-                }
-
-                // Get all target fields from the row
-                targets = nextFormRows.find('> [' + options.targetIdentifier + '], > .' + options.helpTextIdentifier);
-
-                // If there are no form fields
-                if (targets.length === 0) {
-                    // Set the targets to be the nextFormRow containers
-                    targets = nextFormRows;
-                }
-
-                // Init toggle
-                toggleFields__applyToggle(condition, targets);
-
-                // If the condition is an option in a select box
-                if (condition.is('option')) {
-                    // The field to watch is the select box
-                    fieldToWatch = condition.parents('select');
-                }
-                // If the condition is a radio button
-                else if (condition.is(':radio')) {
-                    radioNameIdentifier = condition.attr('name');
-                    // The field to watch needs to be aware of the group of radio buttons
-                    fieldToWatch = condition.parents().find('[name^="' + radioNameIdentifier + '"]');
-                } else {
-                    // Otherwise the field should be the element with the condition identifier
-                    fieldToWatch = condition;
-                }
-
-                // Target the on change
-                fieldToWatch.on('change', function() {
-                    // Update toggle
-                    toggleFields__applyToggle(condition, targets);
-                });
-            });
-        }
+        // Initialise plugin
+        this.init();
     }
 
-    /*
-     * Handles the disabled and enabled states
-     * @param {jQueryObject} condition - The trigger for the condition
-     * @param {jQueryObject} targets - The form fields to be toggled
-     */
-    function toggleFields__applyToggle(condition, targets) {
-        var options = $.extend({}, callbacks, defaults, userOptions),
-            conditionContainer;
+    // Avoid Plugin.prototype conflicts
+    $.extend(ToggleFields.prototype, {
 
-        condition = $(condition);
-        targets = $(targets);
+        init: function() {
+            var self = this,
+                conditions = $('[' + self.settings.conditionsIdentifier + ']');
 
-        // Initially disable all targets
-        targets.each(function() {
-            var target = $(this),
-                container = target.parent('[' + options.nextFormRowsIdentifier + ']');
+            if (conditions.length !== 0) {
+                var conditionValues = conditions.attr(self.settings.conditionsIdentifier),
+                    conditionTargets = $(conditionValues);
 
-            // If the condition has been selected
-            if (condition.is(':selected') || condition.is(':checked')) {
+                // Init callback
+                self.settings.initCallback();
 
-                // If the target is an input field
-                if (target.is('input') || target.is('select')) {
-                    // Remove the disable state
-                    target.removeAttr(options.disabledAttr);
-                }
-                // Remove the disabled class to the target
-                target.removeClass(options.disabledClass);
-                // Remove toggle class to the container
-                container.removeClass(options.toggleClass);
-                // Apply recursion when necessary
-                toggleFields__recursive(container);
-                // Call the toggleOffCallback
-                options.toggleOffCallback();
-            } 
-            // If the condition has not been selected
-            else {
-                // Disable the targets if they have not already been disabled
-                if (!target.attr(options.disabledAttr) || !target.hasClass(options.disabledClass)) {
+                // For each condition
+                conditionTargets.each(function() {
+                    var condition = $(this),
+                        fieldToWatch,
+                        nextFormRows = $('[' + self.settings.nextFormRowsIdentifier + ']'),
+                        radioNameIdentifier,
+                        targets,
+                        wrapper = condition.parents('form');
+
+                    // Add an identifier to the condition
+                    condition.attr(self.settings.conditionIdentifier, '');
+
+                    // If specified next form row
+                    if (nextFormRows.length !== 0) {
+                        // Only match next rows for the condition
+                        nextFormRows = nextFormRows.filter('[' + self.settings.nextRowReferenceIdentifier + '="' + condition.attr('id') + '"]');
+                    } else {
+                        // Otherwise we assume the target is the next form row
+                        nextFormRows = condition.parents().next();
+                        // Add the nextFormRowsIdentifier as an attribute
+                        nextFormRows.attr(self.settings.nextFormRowsIdentifier, '');
+                    }
+
+                    // Get all target fields from the row
+                    targets = nextFormRows.find('> [' + self.settings.targetIdentifier + '], > .' + self.settings.helpTextIdentifier);
+
+                    // If there are no form fields
+                    if (targets.length === 0) {
+                        // Set the targets to be the nextFormRow containers
+                        targets = nextFormRows;
+                    }
+
+                    // Init toggle
+                    self.applyToggle(self, condition, targets);
+
+                    // If the condition is an option in a select box
+                    if (condition.is('option')) {
+                        // The field to watch is the select box
+                        fieldToWatch = condition.parents('select');
+                    }
+                    // If the condition is a radio button
+                    else if (condition.is(':radio')) {
+                        radioNameIdentifier = condition.attr('name');
+                        // The field to watch needs to be aware of the group of radio buttons
+                        fieldToWatch = condition.parents().find('[name^="' + radioNameIdentifier + '"]');
+                    } else {
+                        // Otherwise the field should be the element with the condition identifier
+                        fieldToWatch = condition;
+                    }
+
+                    // Target the on change
+                    fieldToWatch.on('change', function() {
+                        // Update toggle
+                        self.applyToggle(self, condition, targets);
+                    });
+                });
+            }
+        },
+        applyToggle: function(self, condition, targets) {
+            /*
+             * Handles the disabled and enabled states
+             * @param {jQueryObject} condition - The trigger for the condition
+             * @param {jQueryObject} targets - The form fields to be toggled
+             */
+            var conditionContainer;
+
+            condition = $(condition);
+            targets = $(targets);
+
+            // Initially disable all targets
+            targets.each(function() {
+                var target = $(this),
+                    container = target.parent('[' + self.settings.nextFormRowsIdentifier + ']');
+
+                // If the condition has been selected
+                if (condition.is(':selected') || condition.is(':checked')) {
+
                     // If the target is an input field
                     if (target.is('input') || target.is('select')) {
-                        // Add a disabled state
-                        target.attr(options.disabledAttr, options.disabledAttr);
+                        // Remove the disable state
+                        target.removeAttr(self.settings.disabledAttr);
                     }
-                    // Add a disabled class
-                    target.addClass(options.disabledClass);
-                    // Apply recursive logic
-                    container.addClass(options.toggleClass);
-                    // Call the toggleOnCallback
-                    options.toggleOnCallback();
+                    // Remove the disabled class to the target
+                    target.removeClass(self.settings.disabledClass);
+                    // Remove toggle class to the container
+                    container.removeClass(self.settings.toggleClass);
+                    // Apply recursion when necessary
+                    self.recursive(self, container);
+                    // Call the toggleOffCallback
+                    self.settings.toggleOffCallback();
+                } 
+                // If the condition has not been selected
+                else {
+                    // Disable the targets if they have not already been disabled
+                    if (!target.attr(self.settings.disabledAttr) || !target.hasClass(self.settings.disabledClass)) {
+                        // If the target is an input field
+                        if (target.is('input') || target.is('select')) {
+                            // Add a disabled state
+                            target.attr(self.settings.disabledAttr, self.settings.disabledAttr);
+                        }
+                        // Add a disabled class
+                        target.addClass(self.settings.disabledClass);
+                        // Apply recursive logic
+                        container.addClass(self.settings.toggleClass);
+                        // Call the toggleOnCallback
+                        self.settings.toggleOnCallback();
+                    }
+
+                    // Apply recursion when necessary
+                    self.recursive(self, container, condition);
+                }
+            });
+        },
+        recursive: function (self, container, condition) {
+            /*
+             * To re-toggle recursive conditions if they have already been met
+             * @param {jQueryObject} container - The containing element of the targets
+             * @param {jQueryObject} condition - The trigger for the condition before recursion
+             */
+            var recursiveIdentifer = 'data-toggle-recursive';
+
+            // If the container is recursive
+            if (container.is('[' + recursiveIdentifer + ']')) {
+                var nextRecursiveContainer = container.next('[' + recursiveIdentifer + ']'),
+                    nextRecursiveTargets =  nextRecursiveContainer.find('[' + self.settings.targetIdentifier + '], .' + self.settings.helpTextIdentifier);
+
+                // If not using the condition before recursion
+                if (!condition) {
+                    // Use the recursive condition
+                    var condition = container.find('[' + self.settings.conditionIdentifier + ']');
                 }
 
-                // Apply recursion when necessary
-                toggleFields__recursive(container, condition);
+                // If there are no form fields
+                if (nextRecursiveTargets.length === 0) {
+                    // Set the targets to be the nextFormRow container
+                    nextRecursiveTargets = nextRecursiveContainer;
+                }
+
+                // Re-run the logic if the recursive conditions have already been met
+                self.applyToggle(self, condition, nextRecursiveTargets);
+            }
+        }
+
+    });
+
+    // Preventing against multiple instantiations
+    $.fn[pluginName] = function(options) {
+
+        return this.each(function() {
+
+            // If the element does not have the plugin data
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                // Bind the plugin to the element via the data method
+                $.data(this, 'plugin_' + pluginName, new ToggleFields(this, options));
             }
         });
-    }
-
-    /*
-     * To re-toggle recursive conditions if they have already been met
-     * @param {jQueryObject} container - The containing element of the targets
-     * @param {jQueryObject} condition - The trigger for the condition before recursion
-     */
-    function toggleFields__recursive(container, condition) {
-        var options = $.extend({}, callbacks, defaults, userOptions),
-            recursiveIdentifer = 'data-toggle-recursive';
-
-        // If the container is recursive
-        if (container.is('[' + recursiveIdentifer + ']')) {
-            var nextRecursiveContainer = container.next('[' + recursiveIdentifer + ']'),
-                nextRecursiveTargets =  nextRecursiveContainer.find('[' + options.targetIdentifier + '], .' + options.helpTextIdentifier);
-
-            // If not using the condition before recursion
-            if (!condition) {
-                // Use the recursive condition
-                var condition = container.find('[' + options.conditionIdentifier + ']');
-            }
-
-            // If there are no form fields
-            if (nextRecursiveTargets.length === 0) {
-                // Set the targets to be the nextFormRow container
-                nextRecursiveTargets = nextRecursiveContainer;
-            }
-
-            // Re-run the logic if the recursive conditions have already been met
-            toggleFields__applyToggle(condition, nextRecursiveTargets);
-        }
-    }
-}
+    };
+})(jQuery, window, document);
